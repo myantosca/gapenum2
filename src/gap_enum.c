e570500ed5e2a51d36e5d68062dcf8106c487f1d
Michael Yantosca <mike@archivarius.net>
2018-04-14 10:31:28 -0500

    Stub out gap functions.

---
+++ src/gap_enum.c
@@ -0,0 +1,16 @@
+#include "gap_enum.h"
+
+rb_gap_tree_t *gap_xfrm(rb_gap_tree_t *gaps, task_t *tasks)
+{
+     return NULL;
+}
+
+rb_gap_t gap_srch(rb_gap_tree_t *gaps, rb_time_t c)
+{
+     return -1;
+}
+
+rb_time_t gap_enum(task_t *tasks, int j, int windows)
+{
+     return -1;
+}
