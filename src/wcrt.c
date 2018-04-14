6ce093e72373c4e1e6057d6a01cb4b34c47367c9
Michael Yantosca <mike@archivarius.net>
2018-04-14 16:07:53 -0500

    First pass implementation of simple program for loading tasksets and evaluating WCRT using the gap enumeration algorithm.

---
+++ src/wcrt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "gap_enum.h"
+
+
+int main(int argc, char *argv[])
+{
+     task_t tasks[32];
+     size_t n = 0;
+     int done = 0;
+     while (scanf("{%ld,%ld,%ld}\n", &(tasks[n].r), &(tasks[n].c), &(tasks[n].p)) == 3) n++;
+     size_t j;
+     rb_time_t rt_j = 0;
+     for (j = n-1; j >= 0; j--)
+     {
+	  rt_j = gap_enum(tasks, n, j, 1);
+	  printf("RT(τ[%ld] = {r=%ld,c=%ld,p=%ld}) = %ld\n",
+		 j, tasks[j].r, tasks[j].c, tasks[j].p, rt_j);
+	  if (rt_j < 0) break;
+     }
+     return (rt_j < 0);
+}
