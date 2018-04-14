be1967cf21e662eac8096edecd580e5a702ec6d6
Michael Yantosca <mike@archivarius.net>
2018-04-14 10:52:37 -0500

    First-pass implementation of gap_srch based on CLR inorder-tree-walk.

--- src/gap_enum.c
+++ src/gap_enum.c
@@ -5,9 +5,25 @@ rb_gap_tree_t *gap_xfrm(rb_gap_tree_t *gaps, task_t *tasks)
      return NULL;
 }
 
+rb_gap_tree_t *_gap_srch(rb_gap_tree_t *gaps, rb_time_t c, rb_gap_t *gap)
+{
+     if (gaps)
+     {
+	  if (gap->entry < 0) _gap_srch(gaps->left, c, gap);
+	  if ((gap->entry < 0) && (c <= gaps->gap.exit - gaps->gap.entry))
+	  {
+	       gap->entry = gaps->gap.entry;
+	       gap->exit = gaps->gap.exit;
+	  }
+	  if (gap->entry < 0) _gap_srch(gaps->right, c, gap);
+     }
+}
+
 rb_gap_t gap_srch(rb_gap_tree_t *gaps, rb_time_t c)
 {
-     return -1;
+     rb_gap_t gap = { -1, 0 };
+     _gap_srch(gaps, c, &gap);
+     return gap;
 }
 
 rb_time_t gap_enum(task_t *tasks, int j, int windows)
