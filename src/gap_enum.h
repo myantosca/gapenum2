d46e8028e2b1337b2db19710a428cdd50418486f
Michael Yantosca <mike@archivarius.net>
2018-04-14 10:31:07 -0500

    Add function declarations for gap enumeration, transformation, and search per Belwal and Cheng.

---
+++ src/gap_enum.h
@@ -0,0 +1,11 @@
+#ifndef __GAP_ENUM_H
+#define __GAP_ENUM_H
+
+#include "task.h"
+#include "rb_gap_tree.h"
+
+rb_gap_tree_t *gap_xfrm(rb_gap_tree_t *gaps, task_t *tasks);
+rb_gap_t gap_srch(rb_gap_tree_t *gaps, rb_time_t c);
+rb_time_t gap_enum(task_t *tasks, int j, int windows);
+
+#endif
