eaa87a59d0910909f77cd99ab347b3a34bd36aa6
Michael Yantosca <mike@archivarius.net>
2018-04-14 17:18:41 -0500

    Set invalid default for RTj in main.

--- src/wcrt.c
+++ src/wcrt.c
@@ -9,9 +9,11 @@ int main(int argc, char *argv[])
      task_t tasks[32];
      size_t n = 0;
      int done = 0;
+     rb_time_t rt_j = -1;
+
      while (scanf("{%ld,%ld,%ld}\n", &(tasks[n].r), &(tasks[n].c), &(tasks[n].p)) == 3) n++;
+
      size_t j;
-     rb_time_t rt_j = 0;
      for (j = n-1; j >= 0; j--)
      {
 	  rt_j = gap_enum(tasks, n, j, 1);
