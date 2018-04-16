51c47ff2597bbc00ed5264ba1614f970f6cbbd8e
Michael Yantosca <mike@archivarius.net>
2018-04-15 23:53:05 -0500

    Made stdout output more machine-readable.

--- src/wcrt.c
+++ src/wcrt.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 #include "gap_enum.h"
@@ -14,6 +15,7 @@ int main(int argc, char *argv[])
      rb_time_t rt_j = -1;
      while (scanf("{%ld,%ld,%ld}\n", &(tasks[n].r), &(tasks[n].c), &(tasks[n].p)) == 3) n++;
 
+     printf("n,j,RT_j,steps,μs,r1,c1,p1,r2,c2,p2,r3,c3,p3,r4,c4,p4,r5,c5,p5,r6,c6,p6,r7,c7,p7,r8,c8,p8\n");
      int j;
      for (j = n-1; j >= 0; j--)
      {
@@ -23,8 +25,17 @@ int main(int argc, char *argv[])
 	  clock_gettime(CLOCK_MONOTONIC, &t_0);
 	  rt_j = gap_enum(tasks, n, j, 1, &comp_steps);
 	  clock_gettime(CLOCK_MONOTONIC, &t_f);
-	  printf("RT(τ[%d] = {r=%ld,c=%ld,p=%ld}) = %ld [%lu/%lu]\n",
-		 j, tasks[j].r, tasks[j].c, tasks[j].p, rt_j, comp_steps, (t_f.tv_sec - t_0.tv_sec) * 1000000 + (ulong)((t_f.tv_nsec - t_0.tv_nsec) * .001));
+	  size_t t_exec = (t_f.tv_sec - t_0.tv_sec) * 1000000 + (size_t)((t_f.tv_nsec - t_0.tv_nsec) * .001);
+	  printf("%lu,%d,%ld,%lu,%lu,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld\n",
+		 n, j+1, rt_j, comp_steps, t_exec,
+		 tasks[0].r, tasks[0].c, tasks[0].p,
+		 tasks[1].r, tasks[1].c, tasks[1].p,
+		 tasks[2].r, tasks[2].c, tasks[2].p,
+		 tasks[3].r, tasks[3].c, tasks[3].p,
+		 tasks[4].r, tasks[4].c, tasks[4].p,
+		 tasks[5].r, tasks[5].c, tasks[5].p,
+		 tasks[6].r, tasks[6].c, tasks[6].p,
+		 tasks[7].r, tasks[7].c, tasks[7].p);
 	  if (rt_j < 0) break;
      }
      return (rt_j < 0);
