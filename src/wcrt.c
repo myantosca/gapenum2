99cc6a6425918eaf695daa0e2e1757b2dc0bf544
Michael Yantosca <mike@archivarius.net>
2018-04-27 00:27:36 -0500

    Refactor to accept one task per line with variable task set sizes indicated by header.

--- src/wcrt.c
+++ src/wcrt.c
@@ -8,36 +8,44 @@
 
 int main(int argc, char *argv[])
 {
-     task_t tasks[8];
-     memset(tasks, 0, sizeof(task_t) * 8);
-     size_t n = 0;
-     int done = 0;
-     rb_time_t rt_j = -1;
-     while (scanf("{%ld,%ld,%ld}\n", &(tasks[n].r), &(tasks[n].c), &(tasks[n].p)) == 3) n++;
-
-     printf("n,j,RT_j,gaps,steps,μs,r1,c1,p1,r2,c2,p2,r3,c3,p3,r4,c4,p4,r5,c5,p5,r6,c6,p6,r7,c7,p7,r8,c8,p8\n");
-     int j;
-     for (j = n-1; j >= 0; j--)
+     int err = 0;
+     size_t m = 1;
+     printf("m,n,j,RT_j,gaps,steps,μs,r1,c1,p1,r2,c2,p2,r3,c3,p3,r4,c4,p4,r5,c5,p5,r6,c6,p6,r7,c7,p7,r8,c8,p8\n");
+     while (!feof(stdin))
      {
-	  size_t comp_steps = 0;
-	  size_t gap_ct = 0;
-	  struct timespec t_0 = {0,0};
-	  struct timespec t_f = {0,0};
-	  clock_gettime(CLOCK_MONOTONIC, &t_0);
-	  rt_j = gap_enum(tasks, n, j, 1, &comp_steps, &gap_ct);
-	  clock_gettime(CLOCK_MONOTONIC, &t_f);
-	  size_t t_exec = (t_f.tv_sec - t_0.tv_sec) * 1000000 + (size_t)((t_f.tv_nsec - t_0.tv_nsec) * .001);
-	  printf("%lu,%d,%ld,%lu,%lu,%lu,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld\n",
-		 n, j+1, rt_j, gap_ct, comp_steps, t_exec,
-		 tasks[0].r, tasks[0].c, tasks[0].p,
-		 tasks[1].r, tasks[1].c, tasks[1].p,
-		 tasks[2].r, tasks[2].c, tasks[2].p,
-		 tasks[3].r, tasks[3].c, tasks[3].p,
-		 tasks[4].r, tasks[4].c, tasks[4].p,
-		 tasks[5].r, tasks[5].c, tasks[5].p,
-		 tasks[6].r, tasks[6].c, tasks[6].p,
-		 tasks[7].r, tasks[7].c, tasks[7].p);
-	  if (rt_j < 0) break;
+	  size_t n = 0;
+	  int j;
+	  scanf("%lu:", &n);
+	  task_t *tasks = malloc(n * sizeof(task_t));
+	  memset(tasks, 0, sizeof(task_t) * n);
+	  for (j = 0; j < n; j++)
+	  {
+	       scanf("{%ld,%ld,%ld}", &(tasks[j].r), &(tasks[j].c), &(tasks[j].p));
+	  }
+
+	  rb_time_t rt_j = -1;
+	  for (j = n-1; j >= 0; j--)
+	  {
+	       size_t comp_steps = 0;
+	       size_t gap_ct = 0;
+	       struct timespec t_0 = {0,0};
+	       struct timespec t_f = {0,0};
+	       clock_gettime(CLOCK_MONOTONIC, &t_0);
+	       rt_j = gap_enum(tasks, n, j, 1, &comp_steps, &gap_ct);
+	       clock_gettime(CLOCK_MONOTONIC, &t_f);
+	       size_t t_exec = (t_f.tv_sec - t_0.tv_sec) * 1000000 + (size_t)((t_f.tv_nsec - t_0.tv_nsec) * .001);
+	       printf("%lu,%lu,%d,%ld,%lu,%lu,%lu", m, n, j+1, rt_j, gap_ct, comp_steps, t_exec);
+	       int jj;
+	       for (jj = 0; jj < n; jj++)
+	       {
+		    printf(",%ld,%ld,%ld", tasks[jj].r, tasks[jj].c, tasks[jj].p);
+	       }
+	       printf("\n");
+	       if (rt_j < 0) break;
+	  }
+	  err |= (rt_j < 0);
+	  //if (tasks) free(tasks);
+	  m++;
      }
-     return (rt_j < 0);
+     return err;
 }
