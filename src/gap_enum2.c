03bfb33c75e67eaf0cce687700f2fe641d353313
Michael Yantosca <mike@archivarius.net>
2018-04-28 11:06:07 -0500

    Renamed functions for refactor version.

--- src/gap_enum2.c
+++ src/gap_enum2.c
@@ -4,7 +4,7 @@
 #include "rb_gap_tree.h"
 #include "gap_enum.h"
 
-rb_gap_tree_t *gap_xfrm(rb_gap_t W, rb_gap_tree_t *gaps, task_t *tasks, int j, size_t *comp_steps)
+rb_gap_tree_t *gap_xfrm2(rb_gap_t W, rb_gap_tree_t *gaps, task_t *tasks, int j, size_t *comp_steps)
 {
      // Get the number of jobs for the task that will occur within W.
      size_t jobs = ceil(((double)W.exit - (double)tasks[j].r)/(double)tasks[j].p);
@@ -68,11 +68,11 @@ rb_gap_tree_t *gap_xfrm(rb_gap_t W, rb_gap_tree_t *gaps, task_t *tasks, int j, s
      return gaps;
 }
 
-void _gap_srch(rb_gap_tree_t *T, rb_gap_node_t *X, rb_time_t c, rb_gap_t *gap, size_t *comp_steps)
+void _gap_srch2(rb_gap_tree_t *T, rb_gap_node_t *X, rb_time_t c, rb_gap_t *gap, size_t *comp_steps)
 {
      if (X != T->nil)
      {
-	  if (gap->entry < 0) _gap_srch(T, X->left, c, gap, comp_steps);
+	  if (gap->entry < 0) _gap_srch2(T, X->left, c, gap, comp_steps);
 	  if (gap->entry < 0)
 	  {
 	       if (c <= X->gap.exit - X->gap.entry)
@@ -86,18 +86,18 @@ void _gap_srch(rb_gap_tree_t *T, rb_gap_node_t *X, rb_time_t c, rb_gap_t *gap, s
 	  {
 	       (*comp_steps)++;
 	  }
-	  if (gap->entry < 0) _gap_srch(T, X->right, c, gap, comp_steps);
+	  if (gap->entry < 0) _gap_srch2(T, X->right, c, gap, comp_steps);
      }
 }
 
-rb_gap_t gap_srch(rb_gap_tree_t *gaps, rb_time_t c, size_t *comp_steps)
+rb_gap_t gap_srch2(rb_gap_tree_t *gaps, rb_time_t c, size_t *comp_steps)
 {
      rb_gap_t gap = { -1, 0 };
-     _gap_srch(gaps, gaps->root, c, &gap, comp_steps);
+     _gap_srch2(gaps, gaps->root, c, &gap, comp_steps);
      return gap;
 }
 
-rb_time_t gap_enum(task_t *tasks, size_t n, size_t j, int windows, size_t *comp_steps, size_t *gap_ct)
+rb_time_t gap_enum2(task_t *tasks, size_t n, size_t j, int windows, size_t *comp_steps, size_t *gap_ct)
 {
      rb_time_t w = (rb_time_t)ceil((double)tasks[j].p/(double)windows);
      rb_time_t L = w;
@@ -111,7 +111,7 @@ rb_time_t gap_enum(task_t *tasks, size_t n, size_t j, int windows, size_t *comp_
 	  for (i = n-1; i > j; i--)
 	  {
 	       (*comp_steps)++;
-	       rb_gap_tree_t *gaps2 = gap_xfrm((rb_gap_t){ 0, L }, gaps, tasks, i, comp_steps);
+	       rb_gap_tree_t *gaps2 = gap_xfrm2((rb_gap_t){ 0, L }, gaps, tasks, i, comp_steps);
 	       if (!gaps2)
 	       {
 		    *gap_ct = rb_size(gaps);
@@ -122,7 +122,7 @@ rb_time_t gap_enum(task_t *tasks, size_t n, size_t j, int windows, size_t *comp_
 		    return -1;
 	       }
 	  }
-	  rb_gap_t gap = gap_srch(gaps, tasks[j].c, comp_steps);
+	  rb_gap_t gap = gap_srch2(gaps, tasks[j].c, comp_steps);
 	  rb_time_t rt_j = -1;
 	  if (gap.entry >= 0) rt_j = gap.entry + tasks[j].c;
 	  if (rt_j < tasks[j].p)
--- src/gap_enum2.h
+++ src/gap_enum2.h
@@ -4,8 +4,8 @@
 #include "task.h"
 #include "rb_gap_tree.h"
 
-rb_gap_tree_t *gap_xfrm(rb_gap_t W, rb_gap_tree_t *gaps, task_t *tasks, int j, size_t *comp_steps);
-rb_gap_t gap_srch(rb_gap_tree_t *gaps, rb_time_t c, size_t *comp_steps);
-rb_time_t gap_enum(task_t *tasks, size_t n, size_t j, int windows, size_t *comp_steps, size_t *gap_ct);
+rb_gap_tree_t *gap_xfrm2(rb_gap_t W, rb_gap_tree_t *gaps, task_t *tasks, int j, size_t *comp_steps);
+rb_gap_t gap_srch2(rb_gap_tree_t *gaps, rb_time_t c, size_t *comp_steps);
+rb_time_t gap_enum2(task_t *tasks, size_t n, size_t j, int windows, size_t *comp_steps, size_t *gap_ct);
 
 #endif
